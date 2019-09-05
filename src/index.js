613580d0b937e70a1bdf341925e17ee0c4f79366
Jakub Jirutka <jakub@jirutka.cz>
2019-09-06 00:36:14 +0200

    First refactoring

--- src/index.js
+++ src/index.js
@@ -3,34 +3,11 @@ import ansiUp from 'ansi_up'
 import jsdom from 'jsdom'
 import marked from 'marked'
 
-const root = this
 const VERSION = '0.4.2'
 
 const doc = new jsdom.JSDOM().window.document
 
-// Helper functions
-const ident = x => x
-
-function makeElement (tag, classNames) {
-  const el = doc.createElement(tag)
-  el.className = (classNames || []).map(cn => nb.prefix + cn).join(' ')
-  return el
-}
-
-function escapeHTML (raw) {
-  const replaced = raw
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-  return replaced
-}
-
-function joinText (text) {
-  if (text.join) {
-    return text.map(joinText).join('')
-  } else {
-    return text
-  }
-}
+const ident = (x) => x
 
 // Set up `nb` namespace
 const nb = {
@@ -38,11 +15,23 @@ const nb = {
   markdown: marked,
   ansi: ansiUp.ansi_to_html,
   highlighter: ident,
-  VERSION
+  renderMath: doc.renderMathInElement || ident,
+  VERSION,
 }
 
-// Inputs
-class Input {
+function makeElement (tag, classNames) {
+  const el = doc.createElement(tag)
+  el.className = (classNames || []).map(cn => nb.prefix + cn).join(' ')
+  return el
+}
+
+const escapeHTML = (raw) => raw.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+
+function joinText (text) {
+  return text.join ? text.map(joinText).join('') : text
+}
+
+export class Input {
 
   constructor (raw, cell) {
     this.raw = raw
@@ -50,96 +39,110 @@ class Input {
   }
 
   render () {
-    if (!this.raw.length) { return makeElement('div') }
+    if (!this.raw.length) {
+      return makeElement('div')
+    }
     const holder = makeElement('div', ['input'])
     const cell = this.cell
+
     if (typeof cell.number === 'number') {
       holder.setAttribute('data-prompt-number', this.cell.number)
     }
     const preEl = makeElement('pre')
     const codeEl = makeElement('code')
+
     const notebook = cell.worksheet.notebook
     const m = notebook.metadata
     const lang = this.cell.raw.language || m.language || (m.kernelspec && m.kernelspec.language) || (m.language_info && m.language_info.name)
+
     codeEl.setAttribute('data-language', lang)
     codeEl.className = `lang-${lang}`
     codeEl.innerHTML = nb.highlighter(escapeHTML(joinText(this.raw)), preEl, codeEl, lang)
+
     preEl.appendChild(codeEl)
     holder.appendChild(preEl)
-    this.el = holder
+
     return holder
   }
 }
-nb.Input = Input
+
 
 // Outputs and output-renderers
-const imageCreator = format => data => {
+const imageCreator = (format) => (data) => {
   const el = makeElement('img', ['image-output'])
   el.src = `data:image/${format};base64,${joinText(data).replace(/\n/g, '')}`
   return el
 }
 
-nb.display = {}
-nb.display.text = text => {
-  const el = makeElement('pre', ['text-output'])
-  el.innerHTML = escapeHTML(joinText(text))
-  return el
+nb.display = {
+  text: (data) => {
+    const el = makeElement('pre', ['text-output'])
+    el.innerHTML = escapeHTML(joinText(data))
+    return el
+  },
+  html: (data) => {
+    const el = makeElement('div', ['html-output'])
+    el.innerHTML = joinText(data)
+    return el
+  },
+  marked: (data) => nb.display.html(nb.markdown(joinText(data))),
+  svg: (svg) => {
+    const el = makeElement('div', ['svg-output'])
+    el.innerHTML = joinText(svg)
+    return el
+  },
+  latex: (data) => {
+    const el = makeElement('div', ['latex-output'])
+    el.innerHTML = joinText(data)
+    return el
+  },
+  javascript: (data) => {
+    const el = makeElement('script')
+    el.innerHTML = joinText(data)
+    return el
+  },
+  png: imageCreator('png'),
+  jpeg: imageCreator('jpeg'),
 }
-nb.display['text/plain'] = nb.display.text
 
-nb.display.html = html => {
-  const el = makeElement('div', ['html-output'])
-  el.innerHTML = joinText(html)
-  return el
-}
-nb.display['text/html'] = nb.display.html
-
-nb.display.marked = md => nb.display.html(nb.markdown(joinText(md)))
-nb.display['text/markdown'] = nb.display.marked
-
-nb.display.svg = svg => {
-  const el = makeElement('div', ['svg-output'])
-  el.innerHTML = joinText(svg)
-  return el
-}
-nb.display['text/svg+xml'] = nb.display.svg
-nb.display['image/svg+xml'] = nb.display.svg
-
-nb.display.latex = latex => {
-  const el = makeElement('div', ['latex-output'])
-  el.innerHTML = joinText(latex)
-  return el
-}
-nb.display['text/latex'] = nb.display.latex
-
-nb.display.javascript = js => {
-  const el = makeElement('script')
-  el.innerHTML = joinText(js)
-  return el
-}
-nb.display['application/javascript'] = nb.display.javascript
-
-nb.display.png = imageCreator('png')
-nb.display['image/png'] = nb.display.png
-nb.display.jpeg = imageCreator('jpeg')
-nb.display['image/jpeg'] = nb.display.jpeg
+Object.entries({
+  'text/plain': 'text',
+  'text/html': 'html',
+  'text/markdown': 'marked',
+  'text/svg+xml': 'svg',
+  'image/svg+xml': 'svg',
+  'text/latex': 'latex',
+  'application/javascript': 'javascript',
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+}).forEach(([src, tgt]) => {
+  nb.display[src] = nb.display[tgt]
+})
 
 nb.displayPriority = [
-  'png', 'image/png', 'jpeg', 'image/jpeg',
-  'svg', 'image/svg+xml', 'text/svg+xml', 'html', 'text/html',
-  'text/markdown', 'latex', 'text/latex',
-  'javascript', 'application/javascript',
-  'text', 'text/plain'
+  'png',
+  'image/png',
+  'jpeg',
+  'image/jpeg',
+  'svg',
+  'image/svg+xml',
+  'text/svg+xml',
+  'html',
+  'text/html',
+  'text/markdown',
+  'latex',
+  'text/latex',
+  'javascript',
+  'application/javascript',
+  'text',
+  'text/plain',
 ]
 
 function renderDisplayData () {
-  const o = this
-  const formats = nb.displayPriority.filter(d => o.raw.data ? o.raw.data[d] : o.raw[d])
-  const format = formats[0]
-  if (format) {
-    if (nb.display[format]) {
-      return nb.display[format](o.raw[format] || o.raw.data[format])
-    }
+  const format = nb.displayPriority.find(d => this.raw.data ? this.raw.data[d] : this.raw[d])
+
+  if (format && nb.display[format]) {
+    return nb.display[format](this.raw[format] || this.raw.data[format])
   }
   return makeElement('div', ['empty-output'])
 }
@@ -147,11 +150,13 @@ function renderDisplayData () {
 function renderError () {
   const el = makeElement('pre', ['pyerr'])
   const raw = this.raw.traceback.join('\n')
+
   el.innerHTML = nb.highlighter(nb.ansi(escapeHTML(raw)), el)
+
   return el
 }
 
-class Output {
+export class Output {
 
   /* eslint-disable @typescript-eslint/camelcase */
   renderers = {
@@ -160,7 +165,7 @@ class Output {
     pyout: renderDisplayData,
     pyerr: renderError,
     error: renderError,
-    stream: function () {
+    stream: () => {
       const el = makeElement('pre', [(this.raw.stream || this.raw.name)])
       const raw = joinText(this.raw.text)
       el.innerHTML = nb.highlighter(nb.ansi(escapeHTML(raw)), el)
@@ -177,55 +182,48 @@ class Output {
 
   render () {
     const outer = makeElement('div', ['output'])
+
     if (typeof this.cell.number === 'number') {
       outer.setAttribute('data-prompt-number', this.cell.number)
     }
     const inner = this.renderers[this.type].call(this)
     outer.appendChild(inner)
-    this.el = outer
+
     return outer
   }
 }
-nb.Output = Output
 
 // Post-processing
-nb.coalesceStreams = outputs => {
+function coalesceStreams (outputs) {
   if (!outputs.length) { return outputs }
+
   let last = outputs[0]
   const newOutputs = [last]
-  outputs.slice(1).forEach(o => {
-    if (o.raw.output_type === 'stream' &&
-      last.raw.output_type === 'stream' &&
-      o.raw.stream === last.raw.stream) {
-      last.raw.text = last.raw.text.concat(o.raw.text)
+
+  for (const output of outputs.slice(1)) {
+    if (output.raw.output_type === 'stream' && last.raw.output_type === 'stream' && output.raw.stream === last.raw.stream) {
+      last.raw.text = last.raw.text.concat(output.raw.text)
     } else {
-      newOutputs.push(o)
-      last = o
+      newOutputs.push(output)
+      last = output
     }
-  })
+  }
   return newOutputs
 }
 
-class Cell {
+export class Cell {
 
   renderers = {
     markdown () {
       const el = makeElement('div', ['cell', 'markdown-cell'])
       el.innerHTML = nb.markdown(joinText(this.raw.source))
 
-      /* Requires to render KaTeX
-      'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/katex.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/katex.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/contrib/auto-render.min.js',
-      */
-      if (root.renderMathInElement != null) {
-        root.renderMathInElement(el, {delimiters: [
-          {left: '$$', right: '$$', display: true},
-          {left: '\\[', right: '\\]', display: true},
-          {left: '\\(', right: '\\)', display: false},
-          {left: '$', right: '$', display: false}
-        ]})
-      }
+      nb.renderMath(el, { delimiters: [
+        { left: '$$', right: '$$', display: true },
+        { left: '\\[', right: '\\]', display: true },
+        { left: '\\(', right: '\\)', display: false },
+        { left: '$', right: '$', display: false },
+      ] })
 
       return el
     },
@@ -240,81 +238,81 @@ class Cell {
       return el
     },
     code () {
-      const cellEl = makeElement('div', ['cell', 'code-cell'])
-      cellEl.appendChild(this.input.render())
-      const outputEls = this.outputs.forEach(o => {
-        cellEl.appendChild(o.render())
-      })
-      return cellEl
+      const el = makeElement('div', ['cell', 'code-cell'])
+      el.appendChild(this.input.render())
+
+      for (const output of this.outputs) {
+        el.appendChild(output.render())
+      }
+      return el
     },
   }
 
   constructor (raw, worksheet) {
-    const cell = this
-    cell.raw = raw
-    cell.worksheet = worksheet
-    cell.type = raw.cell_type
-    if (cell.type === 'code') {
-      cell.number = raw.prompt_number > -1 ? raw.prompt_number : raw.execution_count
+    this.raw = raw
+    this.worksheet = worksheet
+    this.type = raw.cell_type
+
+    if (this.type === 'code') {
+      this.number = raw.prompt_number > -1 ? raw.prompt_number : raw.execution_count
+
       const source = raw.input || [raw.source]
-      cell.input = new nb.Input(source, cell)
-      const rawOutputs = (cell.raw.outputs || []).map(o => new nb.Output(o, cell))
-      cell.outputs = nb.coalesceStreams(rawOutputs)
+      this.input = new Input(source, this)
+
+      const rawOutputs = (this.raw.outputs || []).map(output => new Output(output, this))
+      this.outputs = coalesceStreams(rawOutputs)
     }
   }
 
   render () {
-    const el = this.renderers[this.type].call(this)
-    this.el = el
-    return el
+    return this.renderers[this.type].call(this)
   }
 }
-nb.Cell = Cell
 
-class Worksheet {
+export class Worksheet {
 
   constructor (raw, notebook) {
-    const worksheet = this
     this.raw = raw
     this.notebook = notebook
-    this.cells = raw.cells.map(c => new nb.Cell(c, worksheet))
+    this.cells = raw.cells.map(cell => new Cell(cell, this))
   }
 
   render () {
-    const worksheetEl = makeElement('div', ['worksheet'])
-    this.cells.forEach(c => {
-      worksheetEl.appendChild(c.render())
-    })
-    this.el = worksheetEl
-    return worksheetEl
+    const el = makeElement('div', ['worksheet'])
+
+    for (const cell of this.cells) {
+      el.appendChild(cell.render())
+    }
+    return el
   }
 }
-nb.Worksheet = Worksheet
 
-class Notebook {
+export class Notebook {
 
-  constructor (raw, config) {
-    const notebook = this
+  constructor (raw) {
     this.raw = raw
-    this.config = config
+
     const meta = this.metadata = raw.metadata || {}
     this.title = meta.title || meta.name
-    const _worksheets = raw.worksheets || [{ cells: raw.cells }]
-    this.worksheets = _worksheets.map(ws => new nb.Worksheet(ws, notebook))
+
+    const worksheets = raw.worksheets || [{ cells: raw.cells }]
+    this.worksheets = worksheets.map(sheet => new Worksheet(sheet, this))
+
     this.sheet = this.worksheets[0]
   }
 
   render () {
-    const notebookEl = makeElement('div', ['notebook'])
-    this.worksheets.forEach(w => {
-      notebookEl.appendChild(w.render())
-    })
-    this.el = notebookEl
-    return notebookEl
+    const el = makeElement('div', ['notebook'])
+
+    for (const sheet of this.worksheets) {
+      el.appendChild(sheet.render())
+    }
+    return el
   }
 }
-nb.Notebook = Notebook
 
-nb.parse = (nbjson, config) => new nb.Notebook(nbjson, config)
+nb.parse = function (nbjson) {
+  return new Notebook(nbjson)
+}
 
 export default nb
