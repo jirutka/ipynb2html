d3b3bddf51481d2eb4a8412073c582b631c50862
Jakub Jirutka <jakub@jirutka.cz>
2019-09-05 22:12:38 +0200

    Use ES6 classes

--- src/index.js
+++ src/index.js
@@ -53,31 +53,35 @@ const nb = {
 }
 
 // Inputs
-nb.Input = function (raw, cell) {
-  this.raw = raw
-  this.cell = cell
-}
+class Input {
 
-nb.Input.prototype.render = function () {
-  if (!this.raw.length) { return makeElement('div') }
-  const holder = makeElement('div', ['input'])
-  const cell = this.cell
-  if (typeof cell.number === 'number') {
-    holder.setAttribute('data-prompt-number', this.cell.number)
+  constructor (raw, cell) {
+    this.raw = raw
+    this.cell = cell
   }
-  const preEl = makeElement('pre')
-  const codeEl = makeElement('code')
-  const notebook = cell.worksheet.notebook
-  const m = notebook.metadata
-  const lang = this.cell.raw.language || m.language || (m.kernelspec && m.kernelspec.language) || (m.language_info && m.language_info.name)
-  codeEl.setAttribute('data-language', lang)
-  codeEl.className = `lang-${lang}`
-  codeEl.innerHTML = nb.highlighter(escapeHTML(joinText(this.raw)), preEl, codeEl, lang)
-  preEl.appendChild(codeEl)
-  holder.appendChild(preEl)
-  this.el = holder
-  return holder
+
+  render () {
+    if (!this.raw.length) { return makeElement('div') }
+    const holder = makeElement('div', ['input'])
+    const cell = this.cell
+    if (typeof cell.number === 'number') {
+      holder.setAttribute('data-prompt-number', this.cell.number)
+    }
+    const preEl = makeElement('pre')
+    const codeEl = makeElement('code')
+    const notebook = cell.worksheet.notebook
+    const m = notebook.metadata
+    const lang = this.cell.raw.language || m.language || (m.kernelspec && m.kernelspec.language) || (m.language_info && m.language_info.name)
+    codeEl.setAttribute('data-language', lang)
+    codeEl.className = `lang-${lang}`
+    codeEl.innerHTML = nb.highlighter(escapeHTML(joinText(this.raw)), preEl, codeEl, lang)
+    preEl.appendChild(codeEl)
+    holder.appendChild(preEl)
+    this.el = holder
+    return holder
+  }
 }
+nb.Input = Input
 
 // Outputs and output-renderers
 const imageCreator = format => data => {
@@ -158,36 +162,42 @@ function renderError () {
   return el
 }
 
-nb.Output = function (raw, cell) {
-  this.raw = raw
-  this.cell = cell
-  this.type = raw.output_type
-}
+class Output {
 
-nb.Output.prototype.renderers = {
-  'display_data': renderDisplayData,
-  'execute_result': renderDisplayData,
-  'pyout': renderDisplayData,
-  'pyerr': renderError,
-  'error': renderError,
-  'stream': function () {
-    const el = makeElement('pre', [(this.raw.stream || this.raw.name)])
-    const raw = joinText(this.raw.text)
-    el.innerHTML = nb.highlighter(nb.ansi(escapeHTML(raw)), el)
-    return el
+  /* eslint-disable @typescript-eslint/camelcase */
+  renderers = {
+    display_data: renderDisplayData,
+    execute_result: renderDisplayData,
+    pyout: renderDisplayData,
+    pyerr: renderError,
+    error: renderError,
+    stream: function () {
+      const el = makeElement('pre', [(this.raw.stream || this.raw.name)])
+      const raw = joinText(this.raw.text)
+      el.innerHTML = nb.highlighter(nb.ansi(escapeHTML(raw)), el)
+      return el
+    },
+  }
+  /* eslint-enable @typescript-eslint/camelcase */
+
+  constructor (raw, cell) {
+    this.raw = raw
+    this.cell = cell
+    this.type = raw.output_type
+  }
+
+  render () {
+    const outer = makeElement('div', ['output'])
+    if (typeof this.cell.number === 'number') {
+      outer.setAttribute('data-prompt-number', this.cell.number)
+    }
+    const inner = this.renderers[this.type].call(this)
+    outer.appendChild(inner)
+    this.el = outer
+    return outer
   }
 }
-
-nb.Output.prototype.render = function () {
-  const outer = makeElement('div', ['output'])
-  if (typeof this.cell.number === 'number') {
-    outer.setAttribute('data-prompt-number', this.cell.number)
-  }
-  const inner = this.renderers[this.type].call(this)
-  outer.appendChild(inner)
-  this.el = outer
-  return outer
-}
+nb.Output = Output
 
 // Post-processing
 nb.coalesceStreams = outputs => {
@@ -207,104 +217,114 @@ nb.coalesceStreams = outputs => {
   return newOutputs
 }
 
-// Cells
-nb.Cell = function (raw, worksheet) {
-  const cell = this
-  cell.raw = raw
-  cell.worksheet = worksheet
-  cell.type = raw.cell_type
-  if (cell.type === 'code') {
-    cell.number = raw.prompt_number > -1 ? raw.prompt_number : raw.execution_count
-    const source = raw.input || [raw.source]
-    cell.input = new nb.Input(source, cell)
-    const rawOutputs = (cell.raw.outputs || []).map(o => new nb.Output(o, cell))
-    cell.outputs = nb.coalesceStreams(rawOutputs)
+class Cell {
+
+  renderers = {
+    markdown () {
+      const el = makeElement('div', ['cell', 'markdown-cell'])
+      el.innerHTML = nb.markdown(joinText(this.raw.source))
+
+      /* Requires to render KaTeX
+      'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/katex.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/katex.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/contrib/auto-render.min.js',
+      */
+      if (root.renderMathInElement != null) {
+        root.renderMathInElement(el, {delimiters: [
+          {left: '$$', right: '$$', display: true},
+          {left: '\\[', right: '\\]', display: true},
+          {left: '\\(', right: '\\)', display: false},
+          {left: '$', right: '$', display: false}
+        ]})
+      }
+
+      return el
+    },
+    heading () {
+      const el = makeElement(`h${this.raw.level}`, ['cell', 'heading-cell'])
+      el.innerHTML = joinText(this.raw.source)
+      return el
+    },
+    raw () {
+      const el = makeElement('div', ['cell', 'raw-cell'])
+      el.innerHTML = joinText(this.raw.source)
+      return el
+    },
+    code () {
+      const cellEl = makeElement('div', ['cell', 'code-cell'])
+      cellEl.appendChild(this.input.render())
+      const outputEls = this.outputs.forEach(o => {
+        cellEl.appendChild(o.render())
+      })
+      return cellEl
+    },
   }
-}
 
-nb.Cell.prototype.renderers = {
-  markdown () {
-    const el = makeElement('div', ['cell', 'markdown-cell'])
-    el.innerHTML = nb.markdown(joinText(this.raw.source))
-
-    /* Requires to render KaTeX
-    'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/katex.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/katex.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/contrib/auto-render.min.js',
-    */
-    if (root.renderMathInElement != null) {
-      root.renderMathInElement(el, {delimiters: [
-        {left: '$$', right: '$$', display: true},
-        {left: '\\[', right: '\\]', display: true},
-        {left: '\\(', right: '\\)', display: false},
-        {left: '$', right: '$', display: false}
-      ]})
+  constructor (raw, worksheet) {
+    const cell = this
+    cell.raw = raw
+    cell.worksheet = worksheet
+    cell.type = raw.cell_type
+    if (cell.type === 'code') {
+      cell.number = raw.prompt_number > -1 ? raw.prompt_number : raw.execution_count
+      const source = raw.input || [raw.source]
+      cell.input = new nb.Input(source, cell)
+      const rawOutputs = (cell.raw.outputs || []).map(o => new nb.Output(o, cell))
+      cell.outputs = nb.coalesceStreams(rawOutputs)
     }
+  }
 
+  render () {
+    const el = this.renderers[this.type].call(this)
+    this.el = el
     return el
-  },
-  heading () {
-    const el = makeElement(`h${this.raw.level}`, ['cell', 'heading-cell'])
-    el.innerHTML = joinText(this.raw.source)
-    return el
-  },
-  raw () {
-    const el = makeElement('div', ['cell', 'raw-cell'])
-    el.innerHTML = joinText(this.raw.source)
-    return el
-  },
-  code () {
-    const cellEl = makeElement('div', ['cell', 'code-cell'])
-    cellEl.appendChild(this.input.render())
-    const outputEls = this.outputs.forEach(o => {
-      cellEl.appendChild(o.render())
-    })
-    return cellEl
   }
 }
+nb.Cell = Cell
 
-nb.Cell.prototype.render = function () {
-  const el = this.renderers[this.type].call(this)
-  this.el = el
-  return el
-}
+class Worksheet {
 
-// Worksheets
-nb.Worksheet = function (raw, notebook) {
-  const worksheet = this
-  this.raw = raw
-  this.notebook = notebook
-  this.cells = raw.cells.map(c => new nb.Cell(c, worksheet))
-  this.render = function () {
+  constructor (raw, notebook) {
+    const worksheet = this
+    this.raw = raw
+    this.notebook = notebook
+    this.cells = raw.cells.map(c => new nb.Cell(c, worksheet))
+  }
+
+  render () {
     const worksheetEl = makeElement('div', ['worksheet'])
-    worksheet.cells.forEach(c => {
+    this.cells.forEach(c => {
       worksheetEl.appendChild(c.render())
     })
     this.el = worksheetEl
     return worksheetEl
   }
 }
+nb.Worksheet = Worksheet
 
-// Notebooks
-nb.Notebook = function (raw, config) {
-  const notebook = this
-  this.raw = raw
-  this.config = config
-  const meta = this.metadata = raw.metadata || {}
-  this.title = meta.title || meta.name
-  const _worksheets = raw.worksheets || [{ cells: raw.cells }]
-  this.worksheets = _worksheets.map(ws => new nb.Worksheet(ws, notebook))
-  this.sheet = this.worksheets[0]
-}
+class Notebook {
 
-nb.Notebook.prototype.render = function () {
-  const notebookEl = makeElement('div', ['notebook'])
-  this.worksheets.forEach(w => {
-    notebookEl.appendChild(w.render())
-  })
-  this.el = notebookEl
-  return notebookEl
+  constructor (raw, config) {
+    const notebook = this
+    this.raw = raw
+    this.config = config
+    const meta = this.metadata = raw.metadata || {}
+    this.title = meta.title || meta.name
+    const _worksheets = raw.worksheets || [{ cells: raw.cells }]
+    this.worksheets = _worksheets.map(ws => new nb.Worksheet(ws, notebook))
+    this.sheet = this.worksheets[0]
+  }
+
+  render () {
+    const notebookEl = makeElement('div', ['notebook'])
+    this.worksheets.forEach(w => {
+      notebookEl.appendChild(w.render())
+    })
+    this.el = notebookEl
+    return notebookEl
+  }
 }
+nb.Notebook = Notebook
 
 nb.parse = (nbjson, config) => new nb.Notebook(nbjson, config)
 
