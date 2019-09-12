9775f6f3bd3323a7a44f0933226a4ef5ba323ecc
Jakub Jirutka <jakub@jirutka.cz>
2019-09-12 18:54:36 +0200

    Rename index.ts to renderer.ts

---
+++ src/renderer.ts
@@ -0,0 +1,256 @@
+// This code is originally based on notebookjs 0.4.2 distributed under the MIT license.
+import { Document, HTMLElement } from 'nodom'
+
+import {
+  Cell,
+  CellType,
+  CodeCell,
+  DisplayData,
+  Error as NbError,
+  ExecuteResult,
+  MarkdownCell,
+  Notebook,
+  Output,
+  OutputType,
+  RawCell,
+  Stream as NbStream,
+} from './nbformat'
+
+
+// TODO: This is only transient type, remove later.
+type Nb = {
+  VERSION: string,
+  prefix: string,
+  markdown: (markup: string) => string,
+  ansi: (text: string) => string,
+  highlighter: (code: string, lang: string) => string,
+  renderMath: (element: HTMLElement, config: { [k: string]: any }) => void,
+  display: DataRenderers,
+  displayPriority: string[],
+  render: (notebook: Notebook) => HTMLElement,
+}
+
+type Attributes = { [k: string]: string }
+
+type DataRenderer = (data: string) => HTMLElement
+type DataRenderers = { [mediaType: string]: DataRenderer }
+
+const VERSION = '0.4.2'
+
+const doc = new Document()
+
+const ident = <T>(x: T): T => x
+
+const katexConfig = {
+  delimiters: [
+    { left: '$$', right: '$$', display: true },
+    { left: '\\[', right: '\\]', display: true },
+    { left: '\\(', right: '\\)', display: false },
+    { left: '$', right: '$', display: false },
+  ],
+}
+
+// Set up `nb` namespace
+const nb: Nb = {
+  prefix: 'nb-',
+  markdown: ident,
+  ansi: ident,
+  highlighter: ident,
+  renderMath: (doc as any).renderMathInElement || ident,
+  VERSION,
+} as any
+
+function el (tag: string, classes?: string[], children?: HTMLElement[] | string): HTMLElement
+function el (tag: string, attrs?: Attributes, children?: HTMLElement[] | string): HTMLElement
+function el (
+  tag: string,
+  classesOrAttrs?: string[] | Attributes,
+  childrenOrHTML?: HTMLElement[] | string,
+): HTMLElement {
+
+  const prefixClassName = (name: string) => name.startsWith('lang-') ? name : nb.prefix + name
+
+  const el = doc.createElement(tag)
+
+  if (Array.isArray(classesOrAttrs)) {
+    el.className = classesOrAttrs.map(prefixClassName).join(' ')
+
+  } else if (classesOrAttrs) {
+    for (const [key, val] of Object.entries(classesOrAttrs)) {
+      if (key === 'class') {
+        el.className = val.split(' ').map(prefixClassName).join(' ')
+      } else {
+        el.setAttribute(key, val)
+      }
+    }
+  }
+  if (Array.isArray(childrenOrHTML)) {
+    childrenOrHTML.forEach(e => el.appendChild(e))
+
+  } else if (childrenOrHTML) {
+    el.innerHTML = childrenOrHTML
+  }
+  return el
+}
+
+const escapeHTML = (raw: string) => raw.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+
+function joinText (text: string | string[]): string {
+  return Array.isArray(text) ? text.map(joinText).join('') : text
+}
+
+// Outputs and output-renderers
+const imageCreator = (format: string) => (data: string | string[]): HTMLElement => {
+  return el('img', {
+    class: 'image-output',
+    src: `data:image/${format};base64,${joinText(data).replace(/\n/g, '')}`,
+  })
+}
+
+nb.display = {
+  'image/png': imageCreator('png'),
+  'image/jpeg': imageCreator('jpeg'),
+  'image/svg+xml': (data) => el('div', ['svg-output'], data),
+  'text/svg+xml': (data) => nb.display['image/svg+xml'](data),
+  'text/html': (data) => el('div', ['html-output'], data),
+  'text/markdown': (data) => nb.display['text/html'](nb.markdown(data)),
+  'text/latex': (data) => el('div', ['latex-output'], data),
+  'application/javascript': (data) => el('script', [], data),
+  'text/plain': (data) => el('pre', ['text-output'], escapeHTML(data)),
+} as DataRenderers
+
+nb.displayPriority = [
+  'image/png',
+  'image/jpeg',
+  'image/svg+xml',
+  'text/svg+xml',
+  'text/html',
+  'text/markdown',
+  'text/latex',
+  'application/javascript',
+  'text/plain',
+]
+
+
+function executionCountAttrs ({ execution_count: count }: CodeCell): Attributes | undefined {
+  return count ? {
+    'data-execution-count': String(count),
+    // Only for backward compatibility with notebook.js.
+    'data-prompt-number': String(count),
+  } : undefined
+}
+
+function renderNotebook (notebook: Notebook): HTMLElement {
+  const children = notebook.cells.map(cell => renderCell(cell, notebook))
+
+  // Class "worksheet" is for backward compatibility with notebook.js.
+  return el('div', ['notebook', 'worksheet'], children)
+}
+nb.render = renderNotebook
+
+function renderCell (cell: Cell, notebook: Notebook): HTMLElement {
+  switch (cell.cell_type) {
+    case CellType.Code: return renderCodeCell(cell, notebook)
+    case CellType.Markdown: return renderMarkdownCell(cell)
+    case CellType.Raw: return renderRawCell(cell)
+  }
+}
+
+function renderMarkdownCell (cell: MarkdownCell): HTMLElement {
+  const div = el('div', ['cell', 'markdown-cell'], nb.markdown(joinText(cell.source)))
+  nb.renderMath(div, katexConfig)
+
+  return div
+}
+
+function renderRawCell (cell: RawCell): HTMLElement {
+  return el('div', ['cell', 'raw-cell'], joinText(cell.source))
+}
+
+function renderCodeCell (cell: CodeCell, notebook: Notebook): HTMLElement {
+  const children = coalesceStreams(cell.outputs || [])
+    .map(output => renderOutput(output, cell))
+
+  children.unshift(renderSource(cell, notebook))
+
+  return el('div', ['cell', 'code-cell'], children)
+}
+
+function renderSource (cell: CodeCell, notebook: Notebook): HTMLElement {
+  if (!cell.source.length) {
+    return el('div')
+  }
+  const m = notebook.metadata
+  const lang = (m.language_info && m.language_info.name) || (m.kernelspec && m.kernelspec.language)
+
+  const html = nb.highlighter(escapeHTML(joinText(cell.source)), lang)
+  const codeEl = el('code', { 'classes': `lang-${lang}`, 'data-language': lang }, html)
+  const preEl = el('pre', [], [codeEl])
+
+  const attrs = {
+    ...executionCountAttrs(cell),
+    // Class "input" is for backward compatibility with notebook.js.
+    class: 'source input',
+  }
+  return el('div', attrs, [preEl])
+}
+
+function renderOutput (output: Output, cell: CodeCell): HTMLElement {
+  const innerEl = (() => {
+    switch (output.output_type) {
+      case OutputType.DisplayData: // fallthrough
+      case OutputType.ExecuteResult: return renderData(output)
+      case OutputType.Stream: return renderStream(output)
+      case OutputType.Error: return renderError(output)
+    }
+  })()
+
+  const attrs = {
+    ...executionCountAttrs(cell),
+    class: 'output',
+  }
+  return el('div', attrs, [innerEl])
+}
+
+function renderData (output: DisplayData | ExecuteResult): HTMLElement {
+  const format = nb.displayPriority.find(d => output.data[d])
+
+  if (format && nb.display[format]) {
+    return nb.display[format](joinText(output.data[format]))
+  }
+  return el('div', ['empty-output'])
+}
+
+function renderError (error: NbError): HTMLElement {
+  const raw = error.traceback.join('\n')
+  const html = nb.ansi(escapeHTML(raw))
+
+  // Class "pyerr" is for backward compatibility with notebook.js.
+  return el('pre', ['error', 'pyerr'], html)
+}
+
+function renderStream (stream: NbStream): HTMLElement {
+  const raw = joinText(stream.text)
+  const html = nb.ansi(escapeHTML(raw))
+
+  return el('pre', [stream.name], html)
+}
+
+function coalesceStreams (outputs: Output[]): Output[] {
+  if (!outputs.length) { return outputs }
+
+  let last = outputs[0]
+  const newOutputs = [last]
+
+  for (const output of outputs.slice(1)) {
+    if (output.output_type === 'stream' && last.output_type === 'stream' && output.name === last.name) {
+      last.text = last.text.concat(...output.text)
+    } else {
+      newOutputs.push(output)
+      last = output
+    }
+  }
+  return newOutputs
+}
+
+export default nb
