a944bf2d27030b2c09aaea3035a951ee6dcf76ec
Jakub Jirutka <jakub@jirutka.cz>
2019-09-17 23:49:21 +0200

    Pre-configure renderer with all external components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import AnsiUp from 'ansi_up'
+import hjs from 'highlightjs'
+import { KatexOptions } from 'katex'
+import { MarkedOptions } from 'marked'
 import { Document } from 'nodom'
 
 import buildElementCreator from './elementCreator'
+import buildMarkdownRenderer from './markdownRenderer'
 import buildRenderer, { Options as RendererOpts, NbRenderer } from './renderer'
 
 
@@ -8,11 +13,31 @@ export { NbRenderer }
 
 export type Options = Partial<RendererOpts> & {
   classPrefix?: string,
+  katexOpts?: KatexOptions,
+  markedOpts?: MarkedOptions,
+}
+
+
+function codeHighlighter (code: string, lang: string): string {
+  return hjs.getLanguage(lang)
+    ? hjs.highlight(lang, code).value
+    : code
 }
 
 export default (opts: Options = {}): NbRenderer => {
   const doc = new Document()
   const elementCreator = buildElementCreator(doc.createElement.bind(doc), opts.classPrefix)
 
-  return buildRenderer({ elementCreator, ...opts })
+  const ansiUp = new AnsiUp()
+  const ansiCodesRenderer = ansiUp.ansi_to_html.bind(ansiUp)
+
+  const markdownRenderer = buildMarkdownRenderer(opts.markedOpts, opts.katexOpts)
+
+  return buildRenderer({
+    ansiCodesRenderer,
+    codeHighlighter,
+    elementCreator,
+    markdownRenderer,
+    ...opts,
+  })
 }
