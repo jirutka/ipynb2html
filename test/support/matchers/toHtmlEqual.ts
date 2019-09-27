1416f80634a5da35800f5caf2a762a35953e6192
Jakub Jirutka <jakub@jirutka.cz>
2019-09-27 03:09:14 +0200

    Improve matcher toHtmlEqual() to support array of elements

--- test/support/matchers/toHtmlEqual.ts
+++ test/support/matchers/toHtmlEqual.ts
@@ -7,14 +7,31 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
     interface Matchers<R> {
-      toHtmlEqual (expected: HTMLElement | string): R,
+      toHtmlEqual (expected: HTMLElement | string | Array<HTMLElement | string>): R,
     }
   }
 }
 
-export function toHtmlEqual (received: HTMLElement, expected: HTMLElement | string): MatcherResult {
-  const receivedStr = received.outerHTML
-  const expectedStr = typeof expected === 'string' ? expected : expected.outerHTML
+function formatArray (items: unknown[]): string {
+  return '[\n' + items.map(x => `  ${x}`).join(',\n') + '\n]'
+}
+
+function stringify (obj: HTMLElement | string): string {
+  return typeof obj === 'string' ? obj : obj.outerHTML
+}
+
+export function toHtmlEqual (
+  received: HTMLElement | HTMLElement[],
+  expected: HTMLElement | string | Array<HTMLElement | string>,
+): MatcherResult {
+
+  const receivedStr = Array.isArray(received)
+    ? formatArray(received.map(stringify))
+    : stringify(received)
+
+  const expectedStr = Array.isArray(expected)
+    ? formatArray(expected.map(stringify))
+    : stringify(expected)
 
   const pass = receivedStr === expectedStr
 
