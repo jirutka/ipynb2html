393e9612210afc4b694920d02bf8bbca9f02c3a6
Jakub Jirutka <jakub@jirutka.cz>
2019-10-07 12:43:17 +0200

    Use ttypescript for TS transpilation

--- jest.config.js
+++ jest.config.js
@@ -60,7 +60,11 @@ module.exports = {
   // globalTeardown: null,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: {
+    'ts-jest': {
+      compiler: 'ttypescript',
+    },
+  },
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // moduleDirectories: [
