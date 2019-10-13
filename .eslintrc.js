jirutka/ipynb2html

commit 0394738a358da4fa1a7ca91aca22c0606b1961df

Replace eslint-import-resolver-typescript with eslint-import-resolver-ts
The former doesn't support multiple tsconfig.json and as I found out
now, some rules didn't work with it at all.

.eslintrc.js
@@ -1,13 +1,15 @@
+const tsconfigs = [
+  'tsconfig.json',
+  'test/tsconfig.json',
+]
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    project: [
-      './tsconfig.json',
-      './test/tsconfig.json',
-    ],
+    project: tsconfigs,
   },
   env: {
     es6: true,
@@ -15,8 +17,10 @@ module.exports = {
   },
   settings: {
     'import/resolver': {
-      // Use eslint-import-resolver-typescript to obey "paths" in tsconfig.json.
-      typescript: {},
+      // Use eslint-import-resolver-ts to obey "paths" in tsconfig.json.
+      ts: {
+        directory: tsconfigs,
+      },
     },
   },
   extends: [
@@ -54,11 +58,11 @@ module.exports = {
     'semi': 'off',
 
     // Import
+    'import/default': 'off',
     'import/newline-after-import': 'warn',
-    'import/order': ['warn', {
-      'groups': [['builtin', 'external']],
-      'newlines-between': 'always-and-inside-groups',
-    }],
+    'import/no-absolute-path': 'error',
+    // This rule disallows using both wildcard and selective imports from the same module.
+    'import/no-duplicates': 'off',
 
     // TypeScript
     '@typescript-eslint/consistent-type-definitions': 'off',

src/index.ts
@@ -1,6 +1,7 @@
 import anser from 'anser'
 import hjs from 'highlightjs'
-import katex, { KatexOptions } from 'katex'
+import * as katex from 'katex'
+import { KatexOptions } from 'katex'
 import { MarkedOptions } from 'marked'
 import { Document, HTMLElement } from 'nodom'
 

src/markdownRenderer.ts
@@ -1,6 +1,8 @@
 import hjs from 'highlightjs'
-import katex, { KatexOptions } from 'katex'
-import marked, { MarkedOptions } from 'marked'
+import * as katex from 'katex'
+import { KatexOptions } from 'katex'
+import * as marked from 'marked'
+import { MarkedOptions } from 'marked'
 
 import { extractMath, restoreMath } from './mathExtractor'
 

src/readNotebookTitle.ts
@@ -1,4 +1,4 @@
-import marked from 'marked'
+import * as marked from 'marked'
 
 import { Notebook, CellType } from './nbformat'
 

commit 9cabcadd4cb2da2efb2e7247be7beff6dc1e7900

Fix wrong named imports and disable corresponding ESLint rule
It doesn't work when the code is bundled using Rollup.js. This problem
was introduced in 0394738a358da4fa1a7ca91aca22c0606b1961df. KaTeX and
Marked have it wrong in type declarations, so ESLint rule
import/no-named-as-default-member is not very helpful here...

.eslintrc.js
@@ -63,6 +63,8 @@ module.exports = {
     'import/no-absolute-path': 'error',
     // This rule disallows using both wildcard and selective imports from the same module.
     'import/no-duplicates': 'off',
+    // Some packages has it wrong in type declarations (e.g. katex, marked).
+    'import/no-named-as-default-member': 'off',
 
     // TypeScript
     '@typescript-eslint/consistent-type-definitions': 'off',

src/index.ts
@@ -1,7 +1,6 @@
 import anser from 'anser'
 import hljs from 'highlightjs'
-import * as katex from 'katex'
-import { KatexOptions } from 'katex'
+import katex, { KatexOptions } from 'katex'
 import { MarkedOptions } from 'marked'
 import { Document, HTMLElement } from 'nodom'
 

src/markdownRenderer.ts
@@ -1,8 +1,6 @@
 import hljs from 'highlightjs'
-import * as katex from 'katex'
-import { KatexOptions } from 'katex'
-import * as marked from 'marked'
-import { MarkedOptions } from 'marked'
+import katex, { KatexOptions } from 'katex'
+import marked, { MarkedOptions } from 'marked'
 
 import { extractMath, restoreMath } from './mathExtractor'
 

src/readNotebookTitle.ts
@@ -1,4 +1,4 @@
-import * as marked from 'marked'
+import marked from 'marked'
 
 import { Notebook, CellType } from './nbformat'
 