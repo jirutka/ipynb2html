37ce69b9d17c915ba4fe050cf91be128eeb32401
Jakub Jirutka <jakub@jirutka.cz>
2019-10-30 23:06:36 +0100

    Add reference stylesheet and use it instead of nbpreview's

--- packages/ipynb2html-cli/src/template.ts
+++ packages/ipynb2html-cli/src/template.ts
@@ -1,3 +1,8 @@
+import { $INLINE_FILE } from 'ts-transformer-inline-file'
+
+const notebookCss = $INLINE_FILE('../../ipynb2html/styles/notebook.css')
+
+
 export default (contents: string, title: string): string => `\
 <!DOCTYPE html>
 <html>
@@ -13,10 +18,6 @@ export default (contents: string, title: string): string => `\
         href="https://cdn.jsdelivr.net/npm/katex@0.11.0/dist/katex.min.css"
         integrity="sha384-BdGj8xC2eZkQaxoQ8nSLefg4AV4/AwB3Fj+8SUSo7pnKP6Eoy18liIKTPn9oBYNG"
         crossorigin="anonymous">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/gh/jsvine/nbpreview@c54381b/css/vendor/notebook.css"
-        integrity="sha384-8nb3et+RBCn64rFFK06Oie9mX/D8c59Kg5hha4g9uUehuzCn9DFXGPZ94WYEBb9Z"
-        crossorigin="anonymous">
     <style>
       html, body {
         margin: 0;
@@ -29,6 +30,7 @@ export default (contents: string, title: string): string => `\
         padding: 3em 6em 1em 6em;
         background-color: white;
       }
+${notebookCss.slice(0, -1).replace(/\n\n/g, '\n').replace(/^/gm, '      ')}
     </style>
   </head>
   <body>
