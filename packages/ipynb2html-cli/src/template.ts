eeee0decc6aaf07dcbab708c39aee6e74b1f06e2
Jakub Jirutka <jakub@jirutka.cz>
2019-11-03 20:47:32 +0100

    Extract inline style from template.ts to page.css

--- packages/ipynb2html-cli/src/template.ts
+++ packages/ipynb2html-cli/src/template.ts
@@ -1,6 +1,7 @@
 import { $INLINE_FILE } from 'ts-transformer-inline-file'
 
 const notebookCss = $INLINE_FILE('../../ipynb2html/styles/notebook.css')
+const pageCss = $INLINE_FILE('./page.css')
 
 
 export default (contents: string, title: string): string => `\
@@ -19,29 +20,7 @@ export default (contents: string, title: string): string => `\
         integrity="sha384-BdGj8xC2eZkQaxoQ8nSLefg4AV4/AwB3Fj+8SUSo7pnKP6Eoy18liIKTPn9oBYNG"
         crossorigin="anonymous">
     <style>
-      html, body {
-        margin: 0;
-        padding: 0;
-      }
-      .nb-notebook {
-        max-width: 45rem;
-        margin: 0 auto;
-        padding: 3rem 6rem;
-        background-color: white;
-      }
-      @media (max-width: 900px) {
-        .nb-notebook {
-          padding-left: 5rem;
-          padding-right: 3rem;
-          max-width: none;
-        }
-      }
-      @media (max-width: 768px) {
-        .nb-notebook {
-          padding: 1rem 5% 2rem 5%;
-        }
-      }
-${notebookCss.slice(0, -1).replace(/\n\n/g, '\n').replace(/^/gm, '      ')}
+${(pageCss + notebookCss).slice(0, -1).replace(/\n\n/g, '\n').replace(/^/gm, '      ')}
     </style>
   </head>
   <body>
